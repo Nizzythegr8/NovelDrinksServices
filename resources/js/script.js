c526191dd091fd8a4e767c3f5a905851dbbd1ae9
Nizzythegr8 <78368502+Nizzythegr8@users.noreply.github.com>
2021-02-18 14:26:30 +0100

    added js changed font changed nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,31 +1,18 @@
-/*portfolio*/
-var slideIndex = 1;
-showSlides(slideIndex);
+/*STICKT NAVIGATION*/
+$(document).ready(function() {
+    /*FOR STICKY NAVI*/
+    $('.about-us').waypoint(function(direction) {
+        if (direction == "down") {
+            // $('nav').addClass('.sticky');
+            document.querySelector(".nav").classList.add("sticky")
+                // $('.nav').classList.add("sticky")
+        } else {
+            // $('.nav').removeClass('.sticky');
+            document.querySelector(".nav").classList.remove("sticky")
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+        }
+    }, {
+        offset: '60px'
+    }, 1500);
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-}
-
-/*RESERVE*/
+});
